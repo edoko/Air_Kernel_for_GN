59696cdef6b7404afaecf51ecd68c5ebde0d0a3b
edoko <r_data@naver.com>
2012-06-27 18:56:28 +0900

    Set min clocks 326Mhz

--- arch/arm/mach-omap2/omap2plus-cpufreq.c
+++ arch/arm/mach-omap2/omap2plus-cpufreq.c
@@ -350,7 +350,8 @@ static int __cpuinit omap_cpu_init(struct cpufreq_policy *policy)
 
 	cpufreq_frequency_table_get_attr(freq_table, policy->cpu);
 
-	policy->min = policy->cpuinfo.min_freq;
+//	policy->min = policy->cpuinfo.min_freq;
+	policy->min = 326000;
 	policy->max = 1228800;
 	policy->cur = omap_getspeed(policy->cpu);
 
